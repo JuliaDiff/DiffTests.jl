4a0f80818b9303e92746519d8ffa84935f54105c
Alexey Stukalov <astukalov@seer.bio>
2023-06-21 15:00:14 -0700

    tests: return type inference

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,56 +2,61 @@ using DiffTests
 using Test
 
 n = rand()
-x, y = rand(5, 5), rand(26)
+x, y = rand(4, 6), rand(26)
 A, B = rand(5, 5), rand(5, 5)
 
 # f returns Number
 
 for f in DiffTests.NUMBER_TO_NUMBER_FUNCS
-    @test isa(f(n), Number)
+    @test isa(@inferred(f(n)), Number)
 end
 
 for f in DiffTests.VECTOR_TO_NUMBER_FUNCS
-    @test isa(f(y), Number)
+    @test isa(@inferred(f(y)), Number)
 end
 
 for f in DiffTests.MATRIX_TO_NUMBER_FUNCS
-    @test isa(f(x), Number)
+    @test isa(@inferred(f(A)), Number)
 end
 
 for f in DiffTests.TERNARY_MATRIX_TO_NUMBER_FUNCS
-    @test isa(f(A, B, x), Number)
+    @test isa(@inferred(f(A, B, x)), Number)
 end
 
 # f returns Array
 
 for f in DiffTests.NUMBER_TO_ARRAY_FUNCS
-    @test isa(f(n), Array)
+    @test isa(@inferred(f(n)), Array)
 end
 
 for f in DiffTests.ARRAY_TO_ARRAY_FUNCS
-    @test isa(f(A), Array)
-    @test isa(f(y), Array)
+    @test isa(@inferred(f(x)), Array)
+    @test isa(@inferred(f(y)), Array)
 end
 
 for f in DiffTests.VECTOR_TO_VECTOR_FUNCS
-    @test isa(f(y), Vector)
+    @test isa(@inferred(f(y)), Vector)
 end
 
 for f in DiffTests.MATRIX_TO_MATRIX_FUNCS
-    @test isa(f(A), Matrix)
+    @test isa(@inferred(f(A)), Matrix)
 end
 
 for f in DiffTests.BINARY_MATRIX_TO_MATRIX_FUNCS
-    @test isa(f(A, B), Matrix)
+    @test isa(@inferred(f(A, B)), Matrix)
 end
 
 # f! returns Nothing
 
 for f! in DiffTests.INPLACE_ARRAY_TO_ARRAY_FUNCS
-    @test isa(f!(y, x), Nothing)
+    z = similar(x)
+    @test isa(@inferred(f!(z, x)), Nothing)
 end
 
 for f! in DiffTests.INPLACE_NUMBER_TO_ARRAY_FUNCS
-    @test isa(f!(y, n), Nothing)
+    zx = similar(x)
+    @test isa(@inferred(f!(zx, n)), Nothing)
+
+    zy = similar(y)
+    @test isa(@inferred(f!(zy, n)), Nothing)
 end

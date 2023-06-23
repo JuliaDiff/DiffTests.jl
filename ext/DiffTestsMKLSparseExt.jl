4e9976fb8bbe5b54d05fb985d29fad0ecf6f15a4
Alexey Stukalov <astukalov@seer.bio>
2023-06-23 00:18:09 -0700

    sparse_ldiv(): disable
    / by generic sparse matrix is not supported

--- ext/DiffTestsMKLSparseExt.jl
+++ ext/DiffTestsMKLSparseExt.jl
@@ -4,13 +4,13 @@ import DiffTests: test_matrix, register
 import DiffTests.LinearAlgebra: UpperTriangular, LowerTriangular
 import DiffTests.SparseArrays: sparse
 
-sparse_ldiv(x::AbstractVecOrMat) = sparse(test_matrix(x)) \ x
+#sparse_ldiv(x::AbstractVecOrMat) = sparse(test_matrix(x)) \ x # arbitrary sparse matrix not supported
 sp_utriag_ldiv(x::AbstractVecOrMat) = UpperTriangular(sparse(test_matrix(x))) \ x
 sp_ltriag_ldiv(x::AbstractVecOrMat) = LowerTriangular(sparse(test_matrix(x))) \ x
 
 function __init__()
-    register(:VECTOR_TO_VECTOR_FUNCS, [sparse_ldiv, sp_utriag_ldiv, sp_ltriag_ldiv])
-    register(:MATRIX_TO_MATRIX_FUNCS, [sparse_ldiv, sp_utriag_ldiv, sp_ltriag_ldiv])
+    register(:VECTOR_TO_VECTOR_FUNCS, [sp_utriag_ldiv, sp_ltriag_ldiv])
+    register(:MATRIX_TO_MATRIX_FUNCS, [sp_utriag_ldiv, sp_ltriag_ldiv])
 end
 
 end
